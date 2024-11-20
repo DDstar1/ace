e4a93c39e895ff7edfe3354d833e3bcb3f82e335
DDstar1 <abhuluimendestiny@gmail.com>
2024-11-20 13:22:08 +0100

    kk

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,19 +9,7 @@ export default function NavBar({ styles }) {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className={`${styles} flex relative justify-between items-center p-4 bg-osas-lightBlueGray text-white`}>
-      {/* Hamburger Menu */}
-      <button onClick={toggleMenu} className="flex flex-col space-y-1 z-10">
-        <span className={`block w-6 h-0.5 bg-white transition-all duration-300 ${isOpen ? 'rotate-45 translate-y-1.5' : ''}`} />
-        <span className={`block w-6 h-0.5 bg-white transition-all duration-300 ${isOpen ? 'opacity-0' : ''}`} />
-        <span className={`block w-6 h-0.5 bg-white transition-all duration-300 ${isOpen ? '-rotate-45 -translate-y-1.5' : ''}`} />
-      </button>
-
-      {/* Centered Logo */}
-      <Link href="/" className="absolute left-1/2 -translate-x-1/2 text-xl font-bold">
-        ACE
-      </Link>
-
+    <nav className={`${styles} flex relative items-center p-4 bg-osas-lightBlueGray text-white`}>
       {/* Mobile Menu */}
       <div className={`absolute top-[56px] right-0 w-64 bg-osas-lightBlueGray shadow-lg transition-transform duration-300 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
         <div className="flex flex-col p-4 space-y-4">
@@ -31,6 +19,20 @@ export default function NavBar({ styles }) {
           <Link href="/results" className="hover:text-gray-300">Results</Link>
         </div>
       </div>
+
+      {/* Centered Logo */}
+      <div className="flex-1 flex justify-center">
+        <Link href="/" className="text-xl font-bold">
+          ACE
+        </Link>
+      </div>
+
+      {/* Hamburger Menu */}
+      <button onClick={toggleMenu} className="flex flex-col space-y-1 z-10">
+        <span className={`block w-6 h-0.5 bg-white transition-all duration-300 ${isOpen ? 'rotate-45 translate-y-1.5' : ''}`} />
+        <span className={`block w-6 h-0.5 bg-white transition-all duration-300 ${isOpen ? 'opacity-0' : ''}`} />
+        <span className={`block w-6 h-0.5 bg-white transition-all duration-300 ${isOpen ? '-rotate-45 -translate-y-1.5' : ''}`} />
+      </button>
     </nav>
   );
 }
